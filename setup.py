3338ec8a6720bfa7e8b42743821bb240036f679a
hmtmcse <hmtm.cse@gmail.com>
2021-04-15 13:19:51 +0600

    bismillah

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pf-python-communication',
+    version='1.0',
+    url='https://github.com/problemfighter/pf-python-communication',
+    license='Apache 2.0',
+    author='Touhid Mia',
+    author_email='hmtm.cse@gmail.com',
+    description='Problem Fighter Python Communication helper, such as Websocket, Email, push notification etc',
+    long_description=__doc__,
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    install_requires=[],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache 2.0 License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
